b6bf9925a7521718f5ee08368b00c4ec24ae77f4
bacninhtech88 <dongktc@gmail.com>
2025-09-01 15:18:49 +0700

    thay database

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 # Thông tin kết nối (thay giá trị thực tế của bạn)
-DB_USER = "vie28925_dongktc"
+DB_USER = "vie28925_test"
 DB_PASS = "nguyenvandong"
-DB_HOST = "s88d68.cloudnetwork.vn"   # hoặc "localhost" nếu cùng server
-DB_NAME = "vie28925_template"
+DB_HOST = "103.138.88.18"   # hoặc "localhost" nếu cùng server
+DB_NAME = "vie28925_test"
 
 # Tạo engine (dùng pymysql)
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
